a5fcb47502536218a5fcac13201ebf5d31a0d1ae
acielos <antoniocielos@gmail.com>
2026-05-06 11:29:45 +0200

    v1.5

--- src/main/java/Modelo/VNS.java
+++ src/main/java/Modelo/VNS.java
@@ -14,7 +14,7 @@ public class VNS extends Algoritmo {
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             // Reseteamos
             this.mejorFuncionObjetivo = Double.POSITIVE_INFINITY;
             this.numEvaluaciones = 0;
